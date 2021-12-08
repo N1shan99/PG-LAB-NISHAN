259e274bdf0da6c50c7739c556d8516e7b55f634
N1shan99 <93864054+N1shan99@users.noreply.github.com>
2021-12-08 15:54:28 +0530

    Add files via upload

---
+++ co2/11lambda.py
@@ -0,0 +1,7 @@
+import math
+tarea=lambda b,h:1/2*b*h
+rarea=lambda l,b:l*b
+sarea=lambda a:a*a
+print("area of triangle",tarea(10,20))
+print("area of rectangle",rarea(30,20))
+print("area of square",sarea(2))
---
+++ co2/coc1.py
@@ -0,0 +1,19 @@
+print("IDE stands for Integrated Development Environment. It’s a coding tool which allows 
+you to write, test, and debug your code in an easier way, as they typically offer code 
+completion or code insight by highlighting, resource management, debugging 
+tools,… And even though the IDE is a strictly defined concept, it’s starting to be
+ redefined as other tools such as notebooks start gaining more and more features 
+that traditionally belong to IDEs.
+
+Comparison between IDLE and Thonny
+
+  Thonny is built for education and you can download the latest version from the
+ Thonny website. The download options are at the top right. Thonny looks quite
+ different to IDLE - it has different panels for the editor, the shell and the variables
+ watcher plus (show view) lots of other options as well. It has a powerful debugger
+ built in and other tools which let you manage packages and plugins.
+  The IDLE editor comes built-in with Python and is the one that many tutorials use 
+by default. It's a fine, basic, editor that also has a Python shell built in for interactive
+ programming.When you start Idle up, you get the shell window. This allows you to 
+execute python commands and see the results immediately without having to create 
+a program. This can be useful for trying things out.")
