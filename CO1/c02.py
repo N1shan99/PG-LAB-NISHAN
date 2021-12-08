14aea117cbf1d64c69c17fc024dad3a62faa9ab3
N1shan99 <93864054+N1shan99@users.noreply.github.com>
2021-12-08 16:01:32 +0530

    Add files via upload

--- CO1/c02.py
+++ CO1/c02.py
@@ -1,6 +1,7 @@
-word=str(input("enter  the word:"))
-print("the original string is:"+word)
-print("the vowels are :",end="")
-for i in word:
-    if i in 'aeiouAEIOU':
-        print([i],end="")
+s=int(input("enter start year"))
+e=int(input("enter end year:"))
+if(s<e):
+    print("leap years are:",end="")
+for i in range(s,e):
+    if i%4==0 and i%100!=0:
+        print(i,end=" ")
