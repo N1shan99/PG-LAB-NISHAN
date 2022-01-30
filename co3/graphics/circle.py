fe560454b244845fd91c1533dff12671fa140d2c
N1shan99 <93864054+N1shan99@users.noreply.github.com>
2022-01-30 22:20:45 +0530

    Add files via upload

--- co3/graphics/circle.py
+++ co3/graphics/circle.py
@@ -1,4 +1,4 @@
-def areac(r):
-    print("area of circle is",3.14*r*r)
-    print("perimeter of circle is",2*3.14*r) 
- 
+def area(r):
+    print("Area of Circle: ",3.14*r*r)
+def perimeter(r):
+    print("Perimeter of Circle: ", 2*3.14*r)
---
+++ co3/graphics/rectangle.py
@@ -0,0 +1,5 @@
+def area(l,b):
+    print("Area of Rectangle: ", l*b)
+def perimeter(l,b):
+    print("Perimeter of Rectangle: ", 2*(l+b))
+
